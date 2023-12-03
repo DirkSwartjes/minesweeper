5dffad8bf578126a3c64f6b121ba11a354f3b5a2
Dirk Swartjes <dirkswartjes@gmail.com>
2023-12-03 13:42:01 +0100

    Add tokens for colors

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from '@components';
 import './reset.css';
+import './tokens.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
